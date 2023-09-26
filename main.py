e8406f1915230231297a82fee416d381a465c7ce
Matheus Militão <matheus.silva2533@etec.sp.gov.br>
2023-09-26 18:45:39 -0300

    Commit initial

---
+++ main.py
@@ -0,0 +1,6 @@
+print ("Digite seu nome")
+
+# sempre algo vier do teclado, o input será utilizado, já que ele recebe o que esá sendo escrito
+nome = input(">> ")
+print ("Olá, "+nome+"!")
+print ("Bemm vindo ao pyCharm")
